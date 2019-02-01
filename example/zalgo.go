8101cfe27e261add97f2395b2175a690234d2bba
Stefan VanBuren <vanburenstefan@gmail.com>
2019-02-01 11:32:26 -0500

    Remove loop
    Should never hit, because of the log.Fatal

--- example/zalgo.go
+++ example/zalgo.go
@@ -8,32 +8,31 @@ import (
 func main() {
 	log.SetHandler(zalgolog.Default)
 	log.Info("zalgolog")
-	for {
-		log.WithFields(log.Fields{
-			"animal": "walrus",
-			"size":   "10",
-		}).Info("To invoke the hive-mind representing chaos.")
 
-		log.WithFields(log.Fields{
-			"omg":    true,
-			"number": 122,
-		}).Warn("Invoking the feeling of chaos.")
+	log.WithFields(log.Fields{
+		"animal": "walrus",
+		"size":   "10",
+	}).Info("To invoke the hive-mind representing chaos.")
 
-		log.WithFields(log.Fields{
-			"animal": "walrus",
-			"size":   "10",
-		}).Info("With out order.")
+	log.WithFields(log.Fields{
+		"omg":    true,
+		"number": 122,
+	}).Warn("Invoking the feeling of chaos.")
 
-		log.WithFields(log.Fields{
-			"animal": "walrus",
-			"size":   "9",
-		}).Error("The Nezperdian hive-mind of chaos. Zalgo.")
+	log.WithFields(log.Fields{
+		"animal": "walrus",
+		"size":   "10",
+	}).Info("With out order.")
 
-		log.WithFields(log.Fields{
-			"omg":    true,
-			"number": 100,
-		}).Warn("He who Waits Behind The Wall.")
+	log.WithFields(log.Fields{
+		"animal": "walrus",
+		"size":   "9",
+	}).Error("The Nezperdian hive-mind of chaos. Zalgo.")
 
-		log.Fatal("ZALGO !")
-	}
+	log.WithFields(log.Fields{
+		"omg":    true,
+		"number": 100,
+	}).Warn("He who Waits Behind The Wall.")
+
+	log.Fatal("ZALGO !")
 }
